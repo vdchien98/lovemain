9861ddd7a5dc6345a45c18275ea5876544919503
dangchien <82793130+vdchien98@users.noreply.github.com>
2021-12-07 15:54:54 +0700

    Initial commit

---
+++ CONFIG.js
@@ -0,0 +1,9 @@
+const CONFIGDATA = {
+    titleHeader: 'Làm người yêu anh nhé ',
+    descriptionHeader: 'Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^',
+    buttonYes: 'Đồng Ý',
+    buttonNo: 'Mơ nhé',
+    titleModar: 'Cảm ơn em vì đồng ý',
+    descriptionModar:
+        'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+};
